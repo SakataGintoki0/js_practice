3486ff52abc9431275898556aebbacd1efc193f2
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-05 17:26:02 +0000

    maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+let score = 400
+
+const balance = new Number(1000)
+
+
+// console.log(balance.toFixed(4))
+
+const otherNumber = 23.4667
+
+// console.log(otherNumber.toPrecision(3))
+
+const money = 1000000000
+
+// console.log(money.toLocaleString("en-IN"))
+
+// console.log(Number.MIN_VALUE)
+
+
+// console.log(Math.ceil(Math.random()*6))
+// console.log(Math.round(otherNumber))
+// console.log(Math.pow(2,3))
+
+
+// ((Math.random() * (max-min+1)) + min )
