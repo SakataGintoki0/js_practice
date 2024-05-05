a550850391b9174c38e4abca017b35f4b2cf2fe9
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-05 12:12:37 +0000

    datatpes and operations

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,5 +5,13 @@
 let name = "Yogesh"
 let age = 24
 let isLoggedIn = false
-console.table([name, age, isLoggedIn])
-console.log(typeof(name))
+// console.table([name, age, isLoggedIn])
+// console.log(typeof(name))
+
+//non primitive data type
+
+// let arr = [5, 4, 3]
+// let arr2 = arr
+// console.log(arr2)
+// arr = [5,5,5]
+// console.log(arr2, arr);
---
+++ 01_basics/04_operations.js
@@ -0,0 +1,17 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " People"
+
+// console.log(str1+str2)
+
+// console.log(1+1+"1")
+// console.log("1" + 1 + 1)
+
+// console.log(2> 1)
+
+console.log(2 == "2")
+console.log(2 === "2")
