8c83dd594bd942ac26f941d0ee4e75feca512a42
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-05 05:41:39 +0000

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,29 @@
+let score = "23abc"
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+/*
+"33" => 33
+"abc" => NaN
+null => 0
+true => 1
+false => 0
+undefined => NaN
+*/
+
+let data = 1
+let booleanData = Boolean(data)
+
+console.log(booleanData);
+
+/*
+1 => true
+0 => false
+"string" => true
+"" => false
+*/
