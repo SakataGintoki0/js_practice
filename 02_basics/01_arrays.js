71156d23092f7ccf1d7cc6d6cc073d4673c5d70e
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-06 18:26:00 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,51 @@
+const myArr = ["yogesh",1,2,3,4]
+
+const arrCopy = myArr
+
+
+
+// console.table([myArr, arrCopy]);
+
+myArr[2] = "sewada"
+// console.table([myArr, arrCopy]);
+
+const myHeros = ["shaktimaan", "nagraaj", "doga", "parmanu"]
+
+// console.log(myArr.length);
+
+
+const intArr = [1,2,3,4,5,6]
+
+// intArr.push(2)
+
+// intArr.unshift(0)
+// intArr.shift()
+// console.log(intArr.includes(8))
+
+const newArr = myArr.join()
+
+// console.log(newArr);
+
+// console.log(newArr.replaceAll(",", ""))
+
+// console.log(newArr);
+
+
+// console.log(intArr)
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3)
+console.log("slice: ", myn1)
+console.log("B", myArr)
+
+const myn2 = myArr.splice(1,3)
+console.log("splice: ", myn2)
+console.log("C", myArr)
+
+
+
+
+
+
+
