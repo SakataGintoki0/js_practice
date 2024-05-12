22019630e426a675f1323c744e96c2d1530ff030
SakataGintoki0 <yogeshsewada0@gmail.com>
2024-05-13 02:49:04 +0530

    loops and higher order loops

---
+++ 03_control_flow/03_array_loops.js
@@ -0,0 +1,32 @@
+const arr = [1, 2, 4, 3, 3];
+
+// for (const iterator of arr) {
+//   console.log(iterator);
+// }
+
+const greetings = "Hello world!!!";
+
+// for (const iterator of greetings) {
+//   console.log(iterator);
+// }
+
+const map = new Map();
+
+map.set("IN", "India");
+map.set("USA", "United States of America");
+
+console.log(map);
+
+// for (const [key, values] of map) {
+//     console.log(key)
+// }
+
+const myObj = {
+  game1: "NFS",
+  game2: "Spiderman",
+};
+
+// Not iterable
+// for (const [key, value] of myObj) {
+//   console.log(key);
+// }
---
+++ 03_control_flow/04_objects.js
@@ -0,0 +1,10 @@
+const lang = {
+  js: "javascript",
+  rb: "ruby",
+  cpp: "c++",
+};
+
+for (const key in lang) {
+  console.log(key);
+  console.log(lang.js);
+}
