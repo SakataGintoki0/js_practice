e28cafe7e066d16df0be3f9c7167bd82483c532d
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-23 06:08:22 +0000

    higher order function async handdler

--- 06_advance/03_higher_order_fn.js
+++ 06_advance/03_higher_order_fn.js
@@ -26,4 +26,21 @@ myArr = [1,2,3,4]
 console.log(myArr.myFilter((el) => el === 3))
 
 
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+const asyncHandler2 = function (fn) {
+    async function name(req, res, next) {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            console.log(error)
+            throw new Error
+        }
+    }
+}
