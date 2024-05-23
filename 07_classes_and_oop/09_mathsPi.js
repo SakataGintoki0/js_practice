cd222d631999a522c40a76866c1eb4c08328f853
Yogesh Sewada <80609704+SakataGintoki0@users.noreply.github.com>
2024-05-23 06:30:28 +0000

    Strange case of pi

--- 07_classes_and_oop/09_mathsPi.js
+++ 07_classes_and_oop/09_mathsPi.js
@@ -1,21 +1,30 @@
-const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI");
 
 // console.log(descripter);
 
-const chai = {
-  name: "Ginger chai",
-  price: 250,
-  isAvailable: true,
-};
+// Object.defineProperty(Math, "PI", {
+//   configurable: true,
+//   writable: true
+// })
 
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
-Object.defineProperty(chai, "name", {
-  //   writable: true,
-  enumerable: false,
-});
+// console.log(descripter)
 
-console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
-for (const [key, value] of Object.entries(chai)) {
-  console.log(`${key} : ${value}`);
-}
+// const chai = {
+//   name: "Ginger chai",
+//   price: 250,
+//   isAvailable: true,
+// };
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+// Object.defineProperty(chai, "name", {
+//   //   writable: true,
+//   enumerable: false,
+// });
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// for (const [key, value] of Object.entries(chai)) {
+//   console.log(`${key} : ${value}`);
+// }
+
